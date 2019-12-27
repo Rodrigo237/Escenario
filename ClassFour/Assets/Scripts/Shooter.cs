e17de1b3aff799f8a9789dc5eeba0ca76767c46f
rodrigomoreno <morenorodrigo541@gmail.com>
2019-12-27 00:01:55 -0600

    RodrigoC
    Practica 3 y Salto

--- ClassFour/Assets/Scripts/Shooter.cs
+++ ClassFour/Assets/Scripts/Shooter.cs
@@ -6,8 +6,20 @@ public class Shooter : MonoBehaviour
 {
     public GameObject bulletPrefab;
     private GameObject tmpBullet;
+    public RectTransform rectTransform;
+    private float currentSize;
+    public float dynamicSize;
+    public float speedCamera;
     public float forceBullet;
+    public float minimunSize;
     public bool isActive = false;
+
+     void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+
     // Update is called once per frame
     void Update()
     {
@@ -17,7 +29,28 @@ public class Shooter : MonoBehaviour
                             transform.position, Quaternion.identity);
 
             tmpBullet.transform.up = transform.forward;
+            
             tmpBullet.GetComponent<Rigidbody>().AddForce(transform.forward * forceBullet,ForceMode.Impulse);
         }
+
+        if (isMoving)
+        {
+            currentSize = Mathf.Lerp(currentSize, dynamicSize, speedCamera * Time.deltaTime);
         }
+        else {
+            currentSize = Mathf.Lerp(currentSize, minimunSize, speedCamera * Time.deltaTime);
+        }
+        rectTransform.sizeDelta = new Vector2(currentSize, currentSize);
+    }
+
+
+    bool isMoving {
+        get {
+            if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+                return false;
+            else
+                return true;
+        }
+    
+    }
 }
